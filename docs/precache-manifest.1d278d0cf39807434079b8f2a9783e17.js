b38cc7794ee914f3455f96411611e84ce6af1d7e
Fernando <fernando.telep@gmail.com>
2020-08-07 21:02:51 -0300

    docs agregado

---
+++ docs/precache-manifest.1d278d0cf39807434079b8f2a9783e17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75af54d31c198b149ea0f32ec1d1a034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "341da44b220d373fcf4f",
+    "url": "/static/css/main.a48004b1.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341da44b220d373fcf4f",
+    "url": "/static/js/main.183e8ae5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
